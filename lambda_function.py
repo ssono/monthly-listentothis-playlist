39dda5321c4948f3967cd5cac583b64207d38726
ssono <blusentinel32@gmail.com>
2019-06-13 02:45:33 -0400

    update single playlist each month and add historical ones

--- lambda_function.py
+++ lambda_function.py
@@ -21,7 +21,7 @@ def makePlaylist(userid, authorization, headers):
         12: 'December',
     }
     date = str(datetime.datetime.now()).split('-')
-    m = months[(int(date[1])-1)%12]
+    m = months[(int(date[1])-2)%12]
     playlist_name = m + ' ' + date[0]
     url = "https://api.spotify.com/v1/users/"+userid+"/playlists/"
     payload = '{"name": '+'"'+playlist_name+'"'+'}'
@@ -41,6 +41,33 @@ def addSongs(playlist_id, uri_list, authorization, headers):
     uristring = uristring.replace(':', '%3A')
     url = 'https://api.spotify.com/v1/playlists/'+playlist_id+'/tracks?uris='+uristring
     response = requests.request("POST", url, headers=headers, allow_redirects=False, timeout=None)
+    
+def copySongs(from_id, to_id, authorization, headers):
+    url = "https://api.spotify.com/v1/playlists/"+from_id+"/tracks"
+    fromTracks = requests.request('GET', url, headers=headers, allow_redirects=False, timeout=None).json()['items']
+    fromTracksUris = []
+    
+    for track in fromTracks:
+        fromTracksUris.append(track['track']['uri'])
+        
+    addSongs(to_id, fromTracksUris, authorization, headers)
+    
+def clearLatest(top_50_id, authorization, headers):
+    url = "https://api.spotify.com/v1/playlists/"+top_50_id+"/tracks"
+    
+    latestTracks = requests.request('GET', url, headers=headers, allow_redirects=False, timeout=None)
+    latestTracks = latestTracks.json()['items']
+    latestTracksUris = []
+    
+    for track in latestTracks:
+        latestTracksUris.append({"uri": track['track']['uri']})
+        
+    payload = {"tracks": latestTracksUris}
+    payload = json.dumps(payload)
+    deleteUrl = 'https://api.spotify.com/v1/playlists/'+top_50_id+'/tracks'
+    response = requests.request("DELETE", deleteUrl, headers=headers, data=payload, allow_redirects=False, timeout=None)
+        
+    
 
 def lambda_handler(event, context):
     #Reddit api auth
@@ -59,6 +86,8 @@ def lambda_handler(event, context):
     prep = s_client_id+':'+s_client_secret
     client_credentials = str(base64.b64encode(prep.encode('utf-8')))
     client_credentials = client_credentials[2:-1]
+    user_id = os.environ['user_id']
+    top_50_id = os.environ['top_50_id']
 
     #refresh credentials
     url = 'https://accounts.spotify.com/api/token'
@@ -78,7 +107,17 @@ def lambda_handler(event, context):
     }
 
     #actual work
-    playlist_id = makePlaylist(os.environ['user_id'], authorization, headers)
+    
+    #Create the new historical playlist
+    playlist_id = makePlaylist(user_id, authorization, headers)
+    
+    #copy songs from latest to historical
+    copySongs(top_50_id, playlist_id, authorization, headers)
+    
+    #clear top 50
+    clearLatest(top_50_id, authorization, headers)
+    
+    #refill top 50
     song_uris = []
     for submission in reddit.subreddit('listentothis').top(time_filter='month', limit=100):
         if len(song_uris) == 50:
@@ -92,4 +131,5 @@ def lambda_handler(event, context):
             song_uris.append(getSongUri(search_term, authorization, headers))
         except IndexError:
             pass
-    addSongs(playlist_id, song_uris, authorization, headers)
+    addSongs(top_50_id, song_uris, authorization, headers)
+    
